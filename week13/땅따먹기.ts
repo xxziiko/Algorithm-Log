221460a495efb8cf9ee372fc6a14ab1f04525ad4
xxziiko <xxziiko@gmail.com>
2024-08-15 19:04:01 +0900

    refac: 재귀함수 개선

--- week13/땅따먹기.ts
+++ week13/땅따먹기.ts
@@ -1,13 +1,4 @@
-// https://school.programmers.co.kr/learn/courses/30/lessons/12913
-
-// 각 행에서 하나의 숫자를 선택하는 것 = 전체 문제
-// 각 행에서 하나의 선택하는 부분문제
-
-// 이전 행에서 선택한 열과 동일한 열을 제외하고 최대 값을 선택하는 부분문제
-
-// 이전 행에서 선택한 열에 따라 행에서 선택할 수 있는 최대 값이 달라진다.
-
-function solution(land: number[][]) {
+function solution2(land: number[][]) {
 	const numRows = land.length;
 	const numCols = land[0].length;
 	const arr = Array.from(Array(numRows), () => Array(numCols).fill(0));
@@ -17,7 +8,6 @@ function solution(land: number[][]) {
 	for (const [i, row] of land.entries()) {
 		if (i === 0) continue;
 
-		// 같은 열을 제외한 이전 행의 최댓값을 더한다..
 		for (const [j, num] of row.entries()) {
 			// 같은 열을 제외한 행
 			const newRow = arr[i - 1].filter((num, index) => index !== j);
@@ -26,4 +16,28 @@ function solution(land: number[][]) {
 	}
 
 	return Math.max(...arr[numRows - 1]);
+}
+
+// 계산했던 결과를 저장하고 재사용
+function solution(land: number[][]) {
+	const memo = new Map();
+
+	function go(row: number[][], prevIndex: number): number {
+		if (!row.length) return 0;
+
+		const [first, ...rest] = row;
+		const key = `${first.join("")}-${prevIndex}`;
+		if (memo.has(key)) return memo.get(key);
+
+		const result = first
+			.filter((_, index) => index !== prevIndex)
+			.map((el, index) => el + go(rest, index));
+		// n,  O(n ^ n) , n ^ 3
+
+		const maxResult = Math.max(...result);
+		memo.set(key, maxResult);
+
+		return maxResult;
+	}
+	return go(land, -1);
 }
