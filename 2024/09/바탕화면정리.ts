8f8cb247d78032060e99d514ebca56ccc6b1e9c2
xxziiko <xxziiko@gmail.com>
2024-09-24 15:50:21 +0900

    chore: 프리티어 적용

--- 2024/09/바탕화면정리.ts
+++ 2024/09/바탕화면정리.ts
@@ -1,19 +1,19 @@
 function solution(wallpapers: string[]) {
-	let minX: number | null = null;
-	let maxX: number | null = null;
-	let minY: number | null = null;
-	let maxY: number | null = null;
+  let minX: number | null = null;
+  let maxX: number | null = null;
+  let minY: number | null = null;
+  let maxY: number | null = null;
 
-	for (const [i, wallpaper] of wallpapers.entries()) {
-		for (const [j, char] of [...wallpaper].entries()) {
-			if (char === "#") {
-				if (minX === null || minX > i) minX = i;
-				if (minY === null || minY > j) minY = j;
-				if (maxX === null || maxX < i) maxX = i;
-				if (maxY === null || maxY < j) maxY = j;
-			}
-		}
-	}
+  for (const [i, wallpaper] of wallpapers.entries()) {
+    for (const [j, char] of [...wallpaper].entries()) {
+      if (char === "#") {
+        if (minX === null || minX > i) minX = i;
+        if (minY === null || minY > j) minY = j;
+        if (maxX === null || maxX < i) maxX = i;
+        if (maxY === null || maxY < j) maxY = j;
+      }
+    }
+  }
 
-	return [minX, minY, maxX! + 1, maxY! + 1];
+  return [minX, minY, maxX! + 1, maxY! + 1];
 }
