43c4d5a5aa55a197a4395bbe53dff41d9caf8258
xxziiko <xxziiko@gmail.com>
2025-03-06 10:48:03 +0900

    [Silver I] Title: 작업, Time: 608 ms, Memory: 96196 KB -BaekjoonHub

---
+++ 백준/Silver/21937. 작업/작업.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const run = (inputs) => {
+	const [firstLine, ...rest] = inputs;
+	const [n, m] = firstLine.split(" ").map(Number);
+	const numbers = rest
+		.slice(0, m)
+		.map((number) => number.split(" ").map(Number));
+	const x = Number(rest.slice(-1)[0]);
+	const graph = Array.from({ length: n + 1 }, () => []);
+
+	for (const [from, to] of numbers) {
+		graph[to].push(from);
+	}
+
+	const dfs = (startNode) => {
+		const stack  = [];
+		const visited = Array(n + 1).fill(false);
+		let count = 0;
+
+		stack.push(startNode);
+		visited[startNode] = true;
+
+		while (stack.length > 0) {
+			const node = stack.pop();
+
+			for (const next of graph[node]) {
+				if (!visited[next] && graph[next]) {
+					visited[next] = true;
+					stack.push(next);
+					count++;
+				}
+			}
+		}
+
+		return count;
+	};
+
+	return dfs(x);
+};
+
+console.log(run(input));
