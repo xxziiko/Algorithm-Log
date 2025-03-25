ed75ee6a7731ebb57cc9b04254375ac224d69abe
xxziiko <xxziiko@gmail.com>
2025-03-25 16:24:45 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 7760 ms, Memory: 931100 KB -BaekjoonHub

--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.js
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.js
@@ -1,36 +1,41 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-const N = Number(input[0]);
-const A = [], B = [], C = [], D = [];
+function run() {
+	const N = Number(input[0]);
+	const A = [];
+	const B = [];
+	const C = [];
+	const D = [];
+    
+    // 1. A,B,C,D 각 배열 만들기
+	for (let i = 1; i <= N; i++) {
+		const [a, b, c, d] = input[i].split(" ").map(Number);
+		A.push(a);
+		B.push(b);
+		C.push(c);
+		D.push(d);
+	}
 
-for (let i = 1; i <= N; i++) {
-    const [a, b, c, d] = input[i].split(" ").map(Number);
-    A.push(a);
-    B.push(b);
-    C.push(c);
-    D.push(d);
+	// 2. A+B의 모든 합의 조합을 Map에 저장
+	const sumAB = new Map();
+	for (const a of A) {
+		for (const b of B) {
+			const sum = a + b;
+			sumAB.set(sum, (sumAB.get(sum) ?? 0) + 1);
+		}
+	}
+
+	let count = 0;
+	// 3. C+D의 모든 합을 확인하면서 Map에서 - (C[k] + D[l])를  찾기 (Map 에 존재하면 카운트 증가)
+	for (const c of C) {
+		for (const d of D) {
+			const sum = c + d;
+			if (sumAB.has(-sum)) count += sumAB.get(-sum);
+		}
+	}
+
+	return count;
 }
 
-// 1. A, B의 모든 합을 저장
-const sumAB = new Map();
-for (let i = 0; i < N; i++) {
-    for (let j = 0; j < N; j++) {
-        const sum = A[i] + B[j];
-        sumAB.set(sum, (sumAB.get(sum) || 0) + 1);
-    }
-}
-
-// 2. C, D의 모든 합을 구하고, -sum을 sumAB에서 찾음
-let count = 0;
-for (let i = 0; i < N; i++) {
-    for (let j = 0; j < N; j++) {
-        const sum = C[i] + D[j];
-        if (sumAB.has(-sum)) {
-            count += sumAB.get(-sum);
-        }
-    }
-}
-
-// 정답 출력
-console.log(count);
+console.log(run());
