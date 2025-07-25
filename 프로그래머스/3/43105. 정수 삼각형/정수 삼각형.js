4872c4316fe564ef9a2c29a511573462a7f3523d
xxziiko <xxziiko@gmail.com>
2025-07-25 15:04:45 +0900

    [level 3] Title: 정수 삼각형, Time: 4.34 ms, Memory: 41.2 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,23 +1,22 @@
 function solution(triangle) { 
-    const len = triangle.length
-    
-    // dp = 각 인덱스에 해당하는 계층까지의 누적합?
-    // 1 = 7 7
+    // 1 = 7  7
     // 2 = 15 10
     // 3 = 16 18
+    const len = triangle.length
     const dp = triangle
-      
-    for(let i = 0; i<len ; i++) {
+    
+    for(let i = 0; i < len ; i ++) {
         dp[len-1][i] = triangle[len-1][i]
     }
     
-    for(let i = len-2; i>=0 ; i--) {
-        for(let j = 0; j<= i; j++ ) {
-            dp[i][j] = triangle[i][j] + Math.max(dp[i+1][j], dp[i+1][j+1])
+    
+    // 바닥에서 시작
+    
+    for(let i = len-2; i >=0 ; i --) {
+        for(let j = 0; j <=i; j ++) {
+            dp[i][j] = triangle[i][j] + Math.max(dp[i+1][j], dp[i+1][j+1])            
         }
     }
-    
-
-    //  거쳐간 숫자의 합이 가장 큰 경우
+ 
     return dp[0][0]
 }
