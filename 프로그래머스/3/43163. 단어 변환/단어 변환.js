7c9f56fb9d0c3bbbc704cb4fa32e4ca4434ba1b7
xxziiko <xxziiko@gmail.com>
2025-07-25 13:09:34 +0900

    [level 3] Title: 단어 변환, Time: 0.40 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,29 +1,37 @@
 function solution(begin, target, words) {
-  const visited = { [begin] : 0 };
-  const queue = [begin];
-
-  while(queue.length) {
-    const node = queue.shift();
+    var answer = 0;
+    const visited = {[begin] : 0}
+    const queue = []
     
-    if(node === target) break;
+    queue.push(begin)
     
-    for(const word of words) {
-      if(isConnected(word, node) && !visited[word]) {
-        visited[word] = visited[node] + 1;
-        queue.push(word);
-      }
+    while(queue.length >0 ){
+        const pick = queue.shift()
+        
+        if(pick === target) break
+        
+        for(const word of words) {
+            // 단어 매칭
+            if(isConnected(word, pick) && !visited[word]) {
+                visited[word] = visited[pick] + 1
+                queue.push(word)
+            }
+        }
     }
-  }
-  return visited[target] ?? 0 
+    
+    
+    return visited[target] ?? 0;
 }
 
+// 단어 매칭 로직
 const isConnected = (str1, str2) => {
-  let count = 0;
-  const len = str1.length;
-  
-  for(let i = 0; i < len; i++) {
-    if(str1[i] !== str2[i]) count++;
-  }
-  
-  return count === 1 
+    let count = 0
+    const len = str1.length
+    
+    for(let i = 0; i < len; i++) {
+        if(str1[i] !== str2[i]) count+=1
+    }
+    
+    // 한번에 하나만 바꿀 수 있다
+    return count === 1
 }
