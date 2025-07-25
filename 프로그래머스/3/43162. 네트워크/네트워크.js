4a0b500a23036c765144dcca214f133426ababfc
xxziiko <xxziiko@gmail.com>
2025-07-25 12:50:26 +0900

    [level 3] Title: 네트워크, Time: 0.60 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,33 +1,34 @@
 function solution(n, computers) {
     var answer = 0;
-    const visited = Array(n).fill(false)
-
-    const dfs = (x) => { 
+    const visited = Array(n).fill(0)
+    
+    const dfs = (x) => {
+        // index
         const stack = []
         
         stack.push(x)
         visited[x] = true
-      
         
-        while(stack.length > 0) {
+        while(stack.length >0) {
             const node = stack.pop()
-            // 방문 처리
             
-            for(let next = 0 ; next < n; next ++) {
-                
-            if(node >= 0 && next>= 0 && node < n && next <n && !visited[next] && computers[node][next] ) {
-                visited[next] = true
-                stack.push(next)
+            // next 도 index
+            for(next = 0; next < n; next ++) {
+                if(!visited[next] && computers[node][next] ){
+                    visited[next] =true
+                    stack.push(next)
+                }
+            
             }
-            }
-
         }
+        
         return true
     }
     
-
-        for(let x = 0; x < n; x++){
-            if(!visited[x] && dfs(x, visited)) answer +=1
-        }
+    
+    for(let i = 0; i < n; i++) {
+        if(!visited[i] && dfs(i)) answer +=1
+    }
+    
     return answer;
 }
