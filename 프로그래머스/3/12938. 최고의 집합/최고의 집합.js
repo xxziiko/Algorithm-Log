27dd364b57a2912b32cce58c0cecd50022bc2a88
xxziiko <xxziiko@gmail.com>
2025-07-25 14:26:22 +0900

    [level 3] Title: 최고의 집합, Time: 0.35 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.js
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.js
@@ -1,23 +1,29 @@
 function solution(n, s) {
-    // 자연수 n개로 이루어진 최고의 집합
-    // 각 원소의 합이 s가 되어야 한다. 
-    // 각 원소의 차이가 작을수록 곱이 크다 -> 균등한 원소 구하기 -> 그리디?
+    // 곱이 최대가 되려면? -> 각 원소의 차이가 작아야 한다. 균등.
+    // 예) 1 * 9 vs 4 * 5
+    // base를 동일하게 두고, 나머지를 균등하게 배분
     
     // n = 3, s = 20
+    // 20 * 3 = 6 ... 2
+    // base = 6, remainder = 2
+    // 6 7 7
     
-    //base = 6 6 6
-    // remainder = 2
-    // 6, 6, 8
-    
+    // 최고의 집합이 존재하지 않는 경우 (-1)
     if(n > s) return [-1]
     
     const base = Math.floor(s / n)
     const remainder = s % n
+    // 초기화
     const answer = Array(n).fill(base)
     
-    for(let i = 0; i < remainder ; i++) {
-        answer[n-1-i]++
+    for(let i = 0; i<remainder; i++) {
+        // 뒤에 원소부터 큰 값 (오름차순)
+        const index = n - 1 - i
+        answer[index] +=1
     }
+
     
-    return answer
+    
+    // 곱이 최대가 되는 집합
+    return answer;
 }
